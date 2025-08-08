4a3dd9d8017e39e301ac82747b972a9c2dc16468
orbas1 <ironcrest96@gmail.com>
2025-08-08 02:11:44 +0100

    Expose user id in session

--- lib/auth.ts
+++ lib/auth.ts
@@ -34,4 +34,12 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   session: { strategy: "jwt" },
+  callbacks: {
+    session({ session, token }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
 };
